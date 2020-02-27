ddb2437bdb9d135110ecc3fb5ebb8d611a7ab11d
anupamdahal <48375788+anupamdahal@users.noreply.github.com>
2020-02-26 18:29:03 -0600

    Snipe class skeleton.

---
+++ Snipe.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Snipe
+{
+  public boolean beakIsLong;
+  public boolean bodyIsFat;
+  public boolean longDistanceFlier;
+  public boolean conservativeGenes;
+  public boolean isAlive;
+  public int age;
+  public int energy;
+
+  private float survivalChance;
+  private float foodChance;
+  private Snipe offSpring;
+
+  public float GetFoodChance ()
+  {
+
+  }
+
+  public float GetSurvivalChance ()
+  {
+
+  }
+
+  public Snipe GenerateOffspring ()
+  {
+
+  }
+
+  public void LoseEnergy ()
+  {
+
+  }
+}
