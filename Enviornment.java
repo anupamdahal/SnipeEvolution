0500998bf28deaefd905f472a811ac74fac334a7
anupamdahal <anupamdahal1@gmail.com>
2020-02-27 22:22:37 -0600

    Class complete

---
+++ Enviornment.java
@@ -0,0 +1,11 @@
+public class Enviornment
+{
+    public boolean isPlentiful;
+    public boolean isDangerous;
+
+    public Enviornment(boolean plenty, boolean dangerous){
+        this.isPlentiful = plenty;
+        this.isDangerous = dangerous;
+    }
+
+}
