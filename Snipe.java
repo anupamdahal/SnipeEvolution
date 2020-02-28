715b513b77b2ad4e6373051100e25c466d87163e
anupamdahal <anupamdahal1@gmail.com>
2020-02-27 22:05:45 -0600

    Constructor and functions built. To implement relation between energy and isAlive

--- Snipe.java
+++ Snipe.java
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+import java.util.*;
 
 public class Snipe
 {
@@ -15,28 +9,96 @@ public class Snipe
   public boolean isAlive;
   public int age;
   public int energy;
-
-  private float survivalChance;
+  
+  private int energyRequired;
+  private float predatorSurvivalChance;
   private float foodChance;
   private Snipe offSpring;
 
+  Random random = new Random();
+
+  public Snipe(boolean longbeak, boolean fatbody, boolean longflier, boolean cgenes)
+  {
+    
+    this.isAlive = true;
+    this.energyRequired = 2;
+    
+    this.beakIsLong = longbeak;
+    this.bodyIsFat = fatbody;
+    this.longDistanceFlier = longflier;
+    this.conservativeGenes = cgenes;
+    this.energy = 2;
+    this.age = 0;
+
+    if (this.beakIsLong){
+      this.predatorSurvivalChance -= 0.05;
+      this.foodChance += 0.1;
+    }
+    else{
+      predatorSurvivalChance += 0.05;
+      foodChance -= 0.1;
+    }
+
+    if (this.bodyIsFat){
+      this.predatorSurvivalChance -= 0.1;
+      if (random.nextBoolean()){
+        this.energyRequired = 1;
+      }
+    }
+    else{
+      this.predatorSurvivalChance += 0.1;
+      if (random.nextBoolean() && random.nextBoolean()){
+        this.energyRequired = 3;
+      }
+      
+    }
+
+    if (this.longDistanceFlier){
+      this.foodChance += 0.2;
+      this.predatorSurvivalChance -= 0.1;
+    }
+    else{
+      this.predatorSurvivalChance += 0.2;
+      this.foodChance -= 0.1;
+    }
+  
+  }
+  
   public float GetFoodChance ()
   {
-
+    return foodChance;
   }
 
   public float GetSurvivalChance ()
   {
-
+    return predatorSurvivalChance;
   }
 
   public Snipe GenerateOffspring ()
   {
+    if (this.conservativeGenes){
+      offSpring = new Snipe(this.beakIsLong, this.bodyIsFat, this.longDistanceFlier, this.conservativeGenes);
+    }
+    else{
+      if(random.nextBoolean() || random.nextBoolean()){
+        offSpring = new Snipe(this.beakIsLong, this.bodyIsFat, this.longDistanceFlier, this.conservativeGenes);
+      }
+      else{
+        offSpring = new Snipe(!this.beakIsLong, !this.bodyIsFat, !this.longDistanceFlier, !this.conservativeGenes);
+      }
+    }
 
+    return this.offSpring;
   }
 
-  public void LoseEnergy ()
+  public void LoseEnergy (int e)
   {
-
+   if ((this.energy - e) > 0 ){
+    this.energy -= e;
+   }
+   else{
+     isAlive = false;
+   }
   }
+
 }
