11157dd693dae5c46e534aa749ef65174fa5a869
TorunimaTaposhi <59891229+TorunimaTaposhi@users.noreply.github.com>
2020-03-14 16:07:00 -0500

    Completed

--- CauseOfDeath.java
+++ CauseOfDeath.java
@@ -1,4 +1,5 @@
 
 public enum CauseOfDeath {
-
+	Starvation,Predation;
+	
 }
--- DeathReport.java
+++ DeathReport.java
@@ -4,9 +4,10 @@ public class DeathReport {
 	public int year;
 	public int age;
 	CauseOfDeath cause;
-	public DeathReport(int  year, int age) {
+	public DeathReport(int  year, int age , CauseOfDeath cause) {
 		this.year=year;
 		this.age=age;
+		this.cause=cause;
 	}
 		
 
