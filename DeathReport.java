3b3edd4f5905ab6fdd224ea7060205c4e6c74db4
TorunimaTaposhi <59891229+TorunimaTaposhi@users.noreply.github.com>
2020-03-14 16:04:55 -0500

    Add files via upload

--- DeathReport.java
+++ DeathReport.java
@@ -1,3 +1,13 @@
-public class DeathReport{
-    
+
+public class DeathReport {
+
+	public int year;
+	public int age;
+	CauseOfDeath cause;
+	public DeathReport(int  year, int age) {
+		this.year=year;
+		this.age=age;
+	}
+		
+
 }
